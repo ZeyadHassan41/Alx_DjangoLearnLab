ef91bb8b4a0188413b216d36a539db2075a0447d
zeyadhassan41 <zeyadhassan960@gmail.com>
2025-07-18 18:09:04 +0300

    week10

--- django-models/LibraryProject/relationship_app/admin.py
+++ django-models/LibraryProject/relationship_app/admin.py
@@ -1,7 +1,11 @@
-from django.contrib import admin
-from .models import Author, Book, Library, Librarian
+# relationship_app/views.py
 
-admin.site.register(Author)
-admin.site.register(Book)
-admin.site.register(Library)
-admin.site.register(Librarian)
+from django.contrib.auth.decorators import user_passes_test
+from django.shortcuts import render
+
+def is_admin(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
+
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'admin_view.html')
