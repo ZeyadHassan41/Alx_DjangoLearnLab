e0dfbb2d7c253a7c214d9bb878e90c6509bf0c23
zeyadhassan41 <zeyadhassan960@gmail.com>
2025-07-28 17:36:30 +0300

    add task 1

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
-from .models import Book
+from django.views.generic import DetailView
+from .models import Book, Library
 
-# Function-based View: List all books
 def list_books(request):
-    books = Book.objects.all()  # ✅ Required exact call
-    return render(request, 'relationship_app/list_books.html', {'books': books})  # ✅ Required exact template path
+    books = Book.objects.all()
+    return render(request, 'relationship_app/list_books.html', {'books': books})
+
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'relationship_app/library_detail.html'
+    context_object_name = 'library'
