000d541d123c520fff9487f925deee6b5220d1d9
zeyadhassan41 <zeyadhassan960@gmail.com>
2025-07-28 18:31:54 +0300

    add task 4

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('books/', views.book_list_view, name='book_list'),
     path('libraries/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
 
-    path('books/add/', views.add_book, name='add_book'),
+    path('add_book/', views.add_book, name='add_book'),
     path('books/<int:pk>/edit/', views.edit_book, name='edit_book'),
     path('books/<int:pk>/delete/', views.delete_book, name='delete_book'),
 ]
