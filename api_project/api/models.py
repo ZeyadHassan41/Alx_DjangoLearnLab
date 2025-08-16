53c57b2e7027bcf1654e5d8ae00653907d857e91
zeyadhassan41 <zeyadhassan960@gmail.com>
2025-08-17 01:00:04 +0300

    api Tasks

--- api_project/api/models.py
+++ api_project/api/models.py
@@ -1,9 +1,8 @@
 from django.db import models
 
-class book(models.Model):
+class Book(models.Model):
     title = models.CharField(max_length=200)
-    author = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
 
     def __str__(self):
         return self.title
-    
