56ba1ca7cd5803fb9d883e626ee0287ddce2a8aa
zeyadhassan41 <zeyadhassan960@gmail.com>
2025-08-28 16:36:42 +0300

    django blog task

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,6 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True, null=True)
+    profile_pic = models.ImageField(upload_to="profiles/", blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.user.username}'s profile"
+
+
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,6 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Post
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
+from .forms import RegisterForm
+
 
 def index(request):
     posts = Post.objects.all().order_by('-published_date')
     return render(request, "blog/index.html", {"posts": posts})
+
+
+
+# Registration view
+def register(request):
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # auto login after registration
+            return redirect("profile")
+    else:
+        form = RegisterForm()
+    return render(request, "blog/register.html", {"form": form})
+
+# Profile view
+@login_required
+def profile(request):
+    if request.method == "POST":
+        request.user.email = request.POST.get("email")
+        request.user.save()
+        return redirect("profile")
+    return render(request, "blog/profile.html")
