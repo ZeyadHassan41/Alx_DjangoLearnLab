a872e83536e97f3840a2732616058c775f30427b
zeyadhassan41 <zeyadhassan960@gmail.com>
2025-08-28 16:52:21 +0300

    django blog task

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,27 +1,67 @@
 from django.shortcuts import render, redirect
-from .models import Post
+from django.urls import reverse_lazy
 from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView, View
+from .models import Post
 from .forms import RegisterForm
 
 
-def index(request):
-    posts = Post.objects.all().order_by('-published_date')
-    return render(request, "blog/index.html", {"posts": posts})
+# Homepage -> List all posts
+class PostListView(ListView):
+    model = Post
+    template_name = "blog/index.html"
+    context_object_name = "posts"
+    ordering = ["-published_date"]
 
+
+# Single post detail
+class PostDetailView(DetailView):
+    model = Post
+    template_name = "blog/post_detail.html"
+    context_object_name = "post"
+
+
+# Create new post
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    template_name = "blog/post_form.html"
+    fields = ["title", "content"]  # adjust fields as per your model
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+# Update post
+class PostUpdateView(LoginRequiredMixin, UpdateView):
+    model = Post
+    template_name = "blog/post_form.html"
+    fields = ["title", "content"]
+
+
+# Delete post
+class PostDeleteView(LoginRequiredMixin, DeleteView):
+    model = Post
+    template_name = "blog/post_confirm_delete.html"
+    success_url = reverse_lazy("index")
 
 
 # Registration view
-def register(request):
-    if request.method == "POST":
+class RegisterView(View):
+    def get(self, request):
+        form = RegisterForm()
+        return render(request, "blog/register.html", {"form": form})
+
+    def post(self, request):
         form = RegisterForm(request.POST)
         if form.is_valid():
             user = form.save()
             login(request, user)  # auto login after registration
             return redirect("profile")
-    else:
-        form = RegisterForm()
-    return render(request, "blog/register.html", {"form": form})
+        return render(request, "blog/register.html", {"form": form})
+
 
 # Profile view
 @login_required
