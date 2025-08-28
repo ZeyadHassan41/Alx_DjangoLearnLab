29bc01385d9c6b58a1bc9f3f48e6633a7e7bc5b3
zeyadhassan41 <zeyadhassan960@gmail.com>
2025-08-28 16:57:53 +0300

    django blog task

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,33 +1,40 @@
-from django.shortcuts import render, redirect
-from django.urls import reverse_lazy
-from django.contrib.auth import login
-from django.contrib.auth.decorators import login_required
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView, View
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView,
+)
 from .models import Post
-from .forms import RegisterForm
+
+# Home View
+def home(request):
+    context = {
+        'posts': Post.objects.all()
+    }
+    return render(request, 'blog/home.html', context)
 
 
-# Homepage -> List all posts
+# Class-based view for all posts
 class PostListView(ListView):
     model = Post
-    template_name = "blog/index.html"
-    context_object_name = "posts"
-    ordering = ["-published_date"]
+    template_name = 'blog/home.html'  # <app>/<model>_<viewtype>.html
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+    paginate_by = 5
 
 
 # Single post detail
 class PostDetailView(DetailView):
     model = Post
-    template_name = "blog/post_detail.html"
-    context_object_name = "post"
 
 
 # Create new post
 class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
-    template_name = "blog/post_form.html"
-    fields = ["title", "content"]  # adjust fields as per your model
+    fields = ['title', 'content']
 
     def form_valid(self, form):
         form.instance.author = self.request.user
@@ -35,39 +42,28 @@ class PostCreateView(LoginRequiredMixin, CreateView):
 
 
 # Update post
-class PostUpdateView(LoginRequiredMixin, UpdateView):
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Post
-    template_name = "blog/post_form.html"
-    fields = ["title", "content"]
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
 
 
 # Delete post
-class PostDeleteView(LoginRequiredMixin, DeleteView):
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Post
-    template_name = "blog/post_confirm_delete.html"
-    success_url = reverse_lazy("index")
+    success_url = '/'
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
 
 
-# Registration view
-class RegisterView(View):
-    def get(self, request):
-        form = RegisterForm()
-        return render(request, "blog/register.html", {"form": form})
-
-    def post(self, request):
-        form = RegisterForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(request, user)  # auto login after registration
-            return redirect("profile")
-        return render(request, "blog/register.html", {"form": form})
-
-
-# Profile view
-@login_required
-def profile(request):
-    if request.method == "POST":
-        request.user.email = request.POST.get("email")
-        request.user.save()
-        return redirect("profile")
-    return render(request, "blog/profile.html")
+def about(request):
+    return render(request, 'blog/about.html', {'title': 'About'})
